838772ce642449f95f3c5dc1ac2d4f12bce13da2
Sarah Chibane <sarah.chibane@etu.univ-orleans.fr>
2025-10-30 16:20:45 +0100

    implement an 'about' and 'welcome' views with templates

--- book_review_project/bonnes_lectures/views.py
+++ book_review_project/bonnes_lectures/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def about(request):
+    return HttpResponse("Application Bonnes Lectures,developpée en TP de Framework Web par -Sarah CHIBANE- Université d'Orléans, 2025")
