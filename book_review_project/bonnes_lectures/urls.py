f2b709c38f824220172b1fa1886037636da09fc7
Sarah Chibane <sarah.chibane@etu.univ-orleans.fr>
2025-10-30 16:23:30 +0100

    define Book model with all the required fields

--- book_review_project/bonnes_lectures/urls.py
+++ book_review_project/bonnes_lectures/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('about/', views.about, name='about'),
+    path('', views.welcome, name='welcome'),
 ]
--- book_review_project/bonnes_lectures/views.py
+++ book_review_project/bonnes_lectures/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def about(request):
-    return HttpResponse("Application Bonnes Lectures,developpée en TP de Framework Web par -Sarah CHIBANE- Université d'Orléans, 2025")
+    return render(request, 'about.html')
+
+def welcome(request):
+    return render(request, 'welcome.html')
