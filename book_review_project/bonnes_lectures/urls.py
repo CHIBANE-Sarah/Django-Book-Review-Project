f9d0470d1e5978b61bc0eb3116a923e7abd233e5
Sarah Chibane <sarah.chibane@etu.univ-orleans.fr>
2025-10-30 17:15:51 +0100

    add a 'book_list' view to display all books

--- book_review_project/bonnes_lectures/urls.py
+++ book_review_project/bonnes_lectures/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('', views.welcome, name='welcome'),
+      path('book/', views.book_list, name='book_list'), 
 ]
--- book_review_project/bonnes_lectures/views.py
+++ book_review_project/bonnes_lectures/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def about(request):
@@ -6,3 +7,7 @@ def about(request):
 
 def welcome(request):
     return render(request, 'welcome.html')
+
+def book_list(request):
+    books = Book.objects.all().order_by('title')
+    return render(request, 'book_list.html', {'books': books})
