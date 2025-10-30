dba32d31697e04e95d040c15004865b411a3f330
Sarah Chibane <sarah.chibane@etu.univ-orleans.fr>
2025-10-30 22:34:47 +0100

    implement a book creation form and creating new html file for that 'new_book.html'

--- book_review_project/bonnes_lectures/urls.py
+++ book_review_project/bonnes_lectures/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('', views.welcome, name='welcome'),
     path('book/', views.book_list, name='book_list'), 
-    path('book/<int:id>/', views.book_detail, name='book_detail'),  
+    path('book/<int:id>/', views.book_detail, name='book_detail'),
+    path('new_book/', views.new_book, name='new_book'),  
 ]
--- book_review_project/bonnes_lectures/views.py
+++ book_review_project/bonnes_lectures/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Book
+from .forms import BookForm
 
 def about(request):
     return render(request, 'about.html')
@@ -14,3 +16,13 @@ def book_list(request):
 def book_detail(request, id):
     book = Book.objects.get(id = id)
     return render(request, 'book_detail.html', {'book': book})
+
+def new_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'new_book.html', {'form': form})
