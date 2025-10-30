d545284a4f62d0e09b9bf366f955353866df5c9e
Sarah Chibane <sarah.chibane@etu.univ-orleans.fr>
2025-10-30 22:42:31 +0100

    improve book form with year dropdown selection

--- book_review_project/bonnes_lectures/forms.py
+++ book_review_project/bonnes_lectures/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Book
+import datetime
 
 class BookForm(forms.ModelForm):
+    
+    current_year = datetime.date.today().year
+    YEAR_CHOICES = [(y, y) for y in range(1900, current_year + 1)][::-1]  # ordre décroissant
+
+    year = forms.ChoiceField(choices=YEAR_CHOICES)
+
     class Meta:
         model = Book
         fields = ['title', 'publisher', 'year', 'isbn', 'backCover', 'cover']
